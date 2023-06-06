3ae4ad4cd48e64bd744e92b5fed602d49a599bc7
Hyeonjun Park <phjppo0918@gmail.com>
2023-06-06 16:37:38 +0900

    v0.1.5 : add Component Bean

---
+++ src/main/java/org/example/CompHello.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CompHello {
+    public void chello() {
+        System.out.println("Hello checll");
+    }
+}
