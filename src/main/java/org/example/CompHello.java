90ff2438c5ae7e9b7685dff35a033987d10c0930
Hyeonjun Park <phjppo0918@gmail.com>
2023-06-06 16:45:38 +0900

    v0.1.6 : add env parameter

--- src/main/java/org/example/CompHello.java
+++ src/main/java/org/example/CompHello.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CompHello {
-    public void chello() {
-        System.out.println("Hello checll");
+    public void chello(@Value("${jwt.asdf}") String asdf) {
+        System.out.println(asdf);
+        System.out.println("Hello asdfasdf");
     }
 }
