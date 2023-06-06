95e23984dd91fe16e0dd57208815e7b66c9405d5
Hyeonjun Park <phjppo0918@gmail.com>
2023-06-06 17:06:58 +0900

    v0.1.8 : add Bean

--- src/main/java/org/example/Config.java
+++ src/main/java/org/example/Config.java
@@ -1,10 +1,12 @@
 package org.example;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
+    @Bean
     public CompHello compHello(@Value("${jwt.asdf}") String asdf) {
         return new CompHello(asdf);
     }
